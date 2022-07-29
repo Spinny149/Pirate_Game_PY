c1470dca2bf2ed219f376b3d9ded7a6af96332d4
Spinny140 <czechulok@gmail.com>
2022-07-29 15:12:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+import sys
+from settings import *
+from tiles import Tile
+
+pygame.init()
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+test_tile = pygame.sprite.Group(Tile((100, 100), 200))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.QUIT()
+            sys.exit()
+
+    screen.fill('black')
+    test_tile.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
